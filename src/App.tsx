4a5783e6d4ed60af1463c627fa589472adb5ecde
truonghoanghuy98 <truonghoanghuy98@gmail.com>
2025-02-26 15:30:00 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+import React from 'react'
 import './App.css'
 
 function App() {
-  return <>App.tsx</>
+  const theme = React.useMemo(() => createTheme({ palette: { mode: 'light' } }), [])
+
+  return <ThemeProvider theme={theme}>App.tsx</ThemeProvider>
 }
 
 export default App
