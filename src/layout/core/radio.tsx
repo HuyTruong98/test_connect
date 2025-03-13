b93806404524d533e53182e7a998f69c4e5afbcf
HuyTruong98 <truonghoanghuy98@gmail.com>
2025-03-14 00:28:20 +0700

    update

--- src/layout/core/radio.tsx
+++ src/layout/core/radio.tsx
@@ -4,7 +4,31 @@ export function radio(): Components<Omit<Theme, 'components'>> {
   return {
     MuiRadio: {
       styleOverrides: {
-        root: {}
+        root: {
+          '&.Mui-checked': {
+            color: 'transparent'
+          },
+          '&.Mui-checked svg': {
+            fill: 'url(#radio-gradient)'
+          }
+        }
+      }
+    },
+    MuiSvgIcon: {
+      defaultProps: {
+        component: (props) => (
+          <svg {...props} viewBox='0 0 24 24'>
+            <defs>
+              <linearGradient id='radio-gradient' x1='0%' y1='0%' x2='100%' y2='0%'>
+                <stop offset='0%' stopColor='#fe92ed' />
+                <stop offset='33%' stopColor='#b39cf6' />
+                <stop offset='66%' stopColor='#83a3f7' />
+                <stop offset='100%' stopColor='#08a6fe' />
+              </linearGradient>
+            </defs>
+            <circle cx='12' cy='12' r='6' fill='url(#radio-gradient)' />
+          </svg>
+        )
       }
     }
   };
